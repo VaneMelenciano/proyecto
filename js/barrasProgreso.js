2e7cba25c5ea7682703db0a207ae1eb23c7334db
Aarón Cano <aaroncanoc1@gmail.com>
2021-06-10 12:42:22 -0500

    avance2 - tablas

--- js/barrasProgreso.js
+++ js/barrasProgreso.js
@@ -1,32 +1,12 @@
+//Esta funcion ocurre cada que se carga la página "dashboard.html"
 function progreso(){
-    //Esta funcion ocurre cada que se carga la página "dashboard.html"
-    /*
-    Lo que se debe hacer es mandar a las barras de progreso (con id: electiva1, electiva2, electiva 3. En el html linea 140)
-    el porcentaje de lo que lleva acumulado.
-    El la base de datos hay tres electivas, cada una con 7 creditos
-    La primera tiene 5 créditos acumulados.
-    Primero se debe verificar que aún no esté llena la primera electiva.
-    Esto se hace comparando el valor de creditos_acumulados con cretidos, si son iguales está llena y 
-    se manda 100% a la primera electiva, y así sucesivamnete
-
-    Si aún no está llena, se hace lo siguiente:
-
-    Si recuerdan las reglas de 3, esto se hará tomando los datos de la base de datos:
-    de la tabla electiva (e):
-    e.creditos_acumulados*(100/e.creditos) -> este es el porcentaje tomando los datos de la base de datos
-    
-    al final el porcentaje se va pasando a cada electiva de las 3 posibles en el html, tamnto el el valor de style, como escrito
-
-    Para la conexion a la base de datos usen el php que ya tenemos hecho, obvio deben hacer otro php para las consultas
-    a la tabla electiva
-    */
-
     var lugar_barras = document.getElementById("barras_progreso");
     
     $.ajax({
         url: "php/barrasProgreso.php",
         method:'GET',
         data: {
+            accion: 'leer'
         },
         success: function( respuesta ) {
             var objetoJSON = JSON.parse(respuesta);
@@ -39,8 +19,37 @@ function progreso(){
                     lugar_barras.innerHTML += barra;
                     contador_color++;
                 }
-            }
-            alert(objetoJSON.mensaje);
+            }else alert(objetoJSON.mensaje);
         }
-      });
+    });
+}
+
+function leerTablas(){
+    var tabla_ivp = document.getElementById("tabla_inquietudes_vocacionales_propias");
+    var tabla_ep = document.getElementById("tabla_enfasis_en_la_profesion");
+    var tabla_cf = document.getElementById("tabla_complementarias_a_la_formacion");
+
+    $.ajax({
+        url: "php/barrasProgreso.php",
+        method:'GET',
+        data: {
+            accion: 'leerTablas'
+        },
+        success: function( respuesta ) {
+            var objetoJSON = JSON.parse(respuesta);
+            if(objetoJSON.estado == 1){
+                for(denominacion of objetoJSON.denominaciones){
+                    if(denominacion.eje_tematico === "Inquietudes vocacionales propias"){
+                        tabla_ivp.innerHTML += '<tr><th scope="row">'+denominacion.modalidad+'</th><td align="left"><ol type="a"><li>'+denominacion.ejemplos+'</li></ol></td><td>'+denominacion.descripcion+'</td></tr>';
+                    }
+                    else if(denominacion.eje_tematico == "Énfasis en la profesión"){
+                        tabla_ep.innerHTML += '<tr><th scope="row">'+denominacion.modalidad+'</th><td align="left"><ol type="a"><li>'+denominacion.ejemplos+'</li></ol></td><td>'+denominacion.descripcion+'</td></tr>';
+                    }
+                    else if(denominacion.eje_tematico == "Complementarias a la formación"){
+                        tabla_cf.innerHTML += '<tr><th scope="row">'+denominacion.modalidad+'</th><td align="left"><ol type="a"><li>'+denominacion.ejemplos+'</li></ol></td><td>'+denominacion.descripcion+'</td></tr>';
+                    }
+                }
+            } else alert(objetoJSON.mensaje);
+        }
+    });
 }
--- php/barrasProgreso.php
+++ php/barrasProgreso.php
@@ -1,8 +1,19 @@
 <?php
-    include 'conexion.php';
+    if(isset($_POST['accion'])) $accion = $_POST['accion'];        
+    else if(isset($_GET['accion'])) $accion = $_GET['accion'];
 
-    //no incluí switch por ahora
-    leer($conexion);
+    include 'conexion.php';
+    
+    switch($accion){
+        case 'leer':
+            leer($conexion);
+            break;
+        case 'leerTablas':
+            leerTablas($conexion);
+            break;
+        default:
+            break;
+    }
 
     function leer($conexion){
         $respuesta = array();
@@ -25,6 +36,36 @@
                 $rowElectivas["creditos_acumulados"] = $row["creditos_acumulados"];
                 
                 array_push($respuesta["electivas"], $rowElectivas);
+            }
+        }else{
+            $respuesta["estado"] = 0;
+            $respuesta["mensaje"] = "No se encontraron registros";
+        }
+        echo json_encode($respuesta);
+        mysqli_close($conexion);
+    }
+
+    function leerTablas($conexion){
+        $respuesta = array();
+        $Query = "SELECT eje_tematico, modalidad, ejemplos, descripcion FROM denominacion WHERE eje_tematico = 'Inquietudes vocacionales propias' OR eje_tematico = 'Énfasis en la profesión' OR eje_tematico = 'Complementarias a la formación'";
+        
+        $resultado = mysqli_query($conexion, $Query);
+        $num_resultados = mysqli_num_rows($resultado);
+        if($num_resultados > 0){
+            $respuesta["estado"] = 1;
+            $respuesta["mensaje"] = "registros encontrados";
+
+            $respuesta["denominaciones"] = array();
+
+            while($row = mysqli_fetch_array($resultado)){
+                $rowDenominacion = array();
+                
+                $rowDenominacion["eje_tematico"] = $row["eje_tematico"];
+                $rowDenominacion["modalidad"] = $row["modalidad"];
+                $rowDenominacion["ejemplos"] = $row["ejemplos"];
+                $rowDenominacion["descripcion"] = $row["descripcion"];
+
+                array_push($respuesta["denominaciones"], $rowDenominacion);
             }
         }else{
             $respuesta["estado"] = 0;
